7d24a5291d124f775b7a139275aa2423cafbe157
Omosigho Godswill <godswillomosigho@gmail.com>
2024-01-22 02:39:38 +0100

    First commit

---
+++ src/MarvelData.js
@@ -0,0 +1,95 @@
+export const characters = [
+  // Iron Man
+  {
+    name: "Kamado Tanjiro",
+    alias: "Sun Breath Wielder",
+    img1: "./public/anime1.jpg",
+    homeworld: "Earth",
+    abilities: [
+      "Hinokami Kagura breathing style",
+      "Water breathing style ",
+      "Speed",
+    ],
+    team: "Deamon slayers ",
+    description:
+      "Tanjiro a passionate young man who intends to turn his sister back to a human and slay demons to save people",
+  },
+  {
+    name: "Itadori Yuji",
+    alias: "Black Fist User",
+    abilities: ["Speed", "Strength"],
+    team: "Jujutsu tech high students",
+    img1: "./public/anime2.jpg",
+    homeworld: "Earth",
+    description:
+      "Itadori is a guy cursed by his grandfather to protect the weak due to enourmous power and has some difficulties in carrying out the curse",
+  },
+  {
+    name: "Naruto",
+    alias: "The Hidden Leaf Hero",
+    abilities: ["Rasengan", "Tailed beast"],
+    team: "Shinobi world",
+    img1: "./public/anime3.jpg",
+    homeworld: "Earth",
+
+    description:
+      "Naruto is a person hated when he was young for beign a vessel of a tailed beast but later earned the love of everyone and became friends with the tailed beast",
+  },
+  {
+    name: "Light Yagami",
+    alias: "Giver Of Death",
+    abilities: ["Book that decides the death of people "],
+    team: "death group",
+    img1: "./public/anime5.jpg",
+    homeworld: "Earth",
+
+    description:
+      "Light yagami is a psychopath that decides the time and how the death of people will be at the expence of his life",
+  },
+
+  // Spider-Man
+  {
+    name: "Ken Kaneki",
+    alias: "The Haise Sasaki Wielder",
+    abilities: ["strength, ability to change to a dragon"],
+    team: "team of ghouls",
+    img1: "./public/image4.jpg",
+    homeworld: "Earth",
+    description:
+      "Haise Sasaki is a ghoul that seeks lot of power ",
+  },
+
+  {
+    name: "Goku",
+    alias: "The Saiyen god",
+    abilities: ["Ultra instinct Power "],
+    team: "Dragon ball chasers",
+    img1: "./public/anime7.jpg",
+    homeworld: "Earth",
+    description:
+      "goku is a saiyan that contest in battles to get dragon balls ",
+  },
+  {
+    name: "Deku",
+    alias: "The Mighty Hero",
+    abilities: ["Strenght, height"],
+    team: "Hero Academy",
+    img1: "./public/anime8.jpg",
+    homeworld: "Earth",
+    description:
+      "Deku is born in an age in which people have powers and with the absence of power of his he was gifted power by another and hopes to be the best superhero",
+  },
+  {
+    name: "Eren Yeager",
+    alias: "The Attack Titan",
+    abilities: ["Titan form, Regeneration"],
+    team: "Scout Reagents",
+    img1: "./public/anime9.jpg",
+    homeworld: "Earth",
+    description:
+      "Born in aworld of titans,Eren Yeager is a victim of a titan attack which killed his mother.Eren vows to eradicate all titans and try to make the world better ",
+  },
+  
+];
+
+console.log(characters);
